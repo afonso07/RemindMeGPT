4f5b57022ec87ad38e619202c587a54116a115e9
afonso.lopocarvalho@gmail.com <afonso.lopocarvalho@gmail.com>
2023-03-18 13:56:14 +0000

    added reminder interface

--- components/Reminder.tsx
+++ components/Reminder.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import { CheckCircleIcon } from "@heroicons/react/24/solid";
+import { CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/solid";
 
 const Reminder = ({ completed = false }: { completed?: boolean }) => {
   return (
     <div className="transition-all bg-gradient-to-r from-indigo-100 to-indigo-300  shadow-xl shadow-indigo-500 ring-offset-2 ring-blue-200 ring-offset-indigo-500 hover:ring-4 w-full w-1/2 p-4 rounded-lg">
       <div className="flex gap-4">
         <span>
-          <CheckCircleIcon className="w-6 h-6 text-green-500 drop-shadow-lg" />
+          {completed ? (
+            <CheckCircleIcon className="w-7 h-7 text-green-500 drop-shadow-md" />
+          ) : (
+            <XCircleIcon className="w-7 h-7 text-red-500 drop-shadow-md" />
+          )}
         </span>
         <span>Reminder</span>
       </div>
